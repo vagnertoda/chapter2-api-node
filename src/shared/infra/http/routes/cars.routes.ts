cc5d12a6f4d7bd22845e55f567e9b30afae4ae01
Vagner E. Toda <vagner_ederli@adsp.matsuda.com.br>
2022-09-17 10:08:12 -0300

    f-createspecificationcar

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -4,14 +4,24 @@ import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarC
 import { ListCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
+import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
-carsRoutes.post("/", ensureAuthenticated, ensureAdmin, createCarController.handle);
+carsRoutes.post("/", 
+    ensureAuthenticated, 
+    ensureAdmin, 
+    createCarController.handle);
 
 carsRoutes.get("/available", listAvailableCarsController.handle);
+
+carsRoutes.post("/specifications/:id", 
+    ensureAuthenticated, 
+    ensureAdmin,
+    createCarSpecificationController.handle);
 
 export { carsRoutes };
